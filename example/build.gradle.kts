ecaf34c8d6fb9c4f5893f3a37084379e9cf9914b
radsteve <radsteve@radsteve.net>
2024-07-19 13:26:36 +0200

    bump: change version in example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-    implementation("net.radstevee:universes:0.2.0")
+    implementation("net.radstevee:universes:0.2.1")
 }
 
 val targetJavaVersion = 21
